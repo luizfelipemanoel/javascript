7e7a403cb6b1e6a93c8b865bce970dbdca7724e6
Luiz Felipe Manoel <103972051+luizfelipemanoel@users.noreply.github.com>
2022-05-04 16:47:32 -0300

    finalização exercício 14

--- exercicios/aula12 ex/ex014/script.js
+++ exercicios/aula12 ex/ex014/script.js
@@ -1,1 +1,22 @@
-alert('ola')
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        img.src = 'imagem/manha.png'
+        document.body.style.background = '#eeee44'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagem/tarde.png'
+        document.body.style.background = '#ecb349'
+    } else {
+        img.src = 'imagem/noite.png'
+        document.body.style.background = '#3d3c3c'
+    }
+
+
+
+}
+
