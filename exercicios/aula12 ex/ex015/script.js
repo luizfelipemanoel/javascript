b22d0c6d27e39b5895b3335f47afd81ec5b277d3
Luiz Felipe Manoel <103972051+luizfelipemanoel@users.noreply.github.com>
2022-05-11 19:17:16 -0300

    Update script.js

--- exercicios/aula12 ex/ex015/script.js
+++ exercicios/aula12 ex/ex015/script.js
@@ -6,6 +6,24 @@ function verificar(){
     if(fano.value.length == 0 || fano.value > ano ){
         alert('[ERRO] Verifique os dados e tente novamente!')
     } else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number( fano.value)
+        var genero = ''
+        var img = dicument.crateElement('img')
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src', 'imagem/homem-criaca.jpg')
+            }
+          
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+
+        }
+   
      
     }
+    res.getElementsByClassName.textAling = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
 }
